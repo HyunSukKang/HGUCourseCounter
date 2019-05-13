c8dbea9e8e4e3d88054cdf96e28050acf7c8b554
HyunSukKang <21400018@handong.edu>
2019-05-14 03:30:23 +0900

    hw5

--- src/main/java/edu/handong/analysis/datamodel/Student.java
+++ src/main/java/edu/handong/analysis/datamodel/Student.java
@@ -1,13 +1,24 @@
 package edu.handong.analysis.datamodel;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Student {
-	private String name;
+	private String studentId;
+	private ArrayList<Course> coursesTaken;
+	private HashMap<String, Integer> semestersByYearAndSemester;
 	
-	public Student(String name) {
-		this.name = name;
+	public Student(String studentId) {
+		this.studentId = studentId;
 	}
-	
-	public String getName() {
-		return name;
+	public void addCourse(Course newRecord) {
+		
+	}
+	public HashMap<String, Integer> getSemestersByYearAndSemester(){
+		return semestersByYearAndSemester;
+	}
+	public int getNumCourseInNthSementer(int semester) {
+		
+		
 	}
 }
--- src/main/java/edu/handong/analysis/utils/Utils.java
+++ src/main/java/edu/handong/analysis/utils/Utils.java
@@ -4,9 +4,14 @@ import java.util.ArrayList;
 
 public class Utils {
 
-	public static ArrayList<String> getLines(String dataPath, boolean b) {
+	public static ArrayList<String> getLines(String file, boolean removeHeader) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public static void writeAFile(ArrayList<String> lines, String targetFileName) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
