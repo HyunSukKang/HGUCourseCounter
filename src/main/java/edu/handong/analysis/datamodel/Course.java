cdf092a89eb9d8dcbeebff8e79d6bd14244cf67f
HyunSukKang <21400018@handong.edu>
2019-05-14 02:47:43 +0900

    hw5

--- src/main/java/edu/handong/analysis/datamodel/Course.java
+++ src/main/java/edu/handong/analysis/datamodel/Course.java
@@ -1,12 +1,19 @@
 package edu.handong.analysis.datamodel;
 
 public class Course {
+	private String studentId;
+	private String yearMonthGraduated;
+	private String firstMajor;
+	private String secondMajor;
+	private String courseCode;
 	private String courseName;
+	private String courseCredit;
+	private int yearTaken;
+	private int semesterCourseTaken;
 	
-	public Course(String name) {
-		courseName = name;
+	public Course(String line) {
+		
 	}
-	public String getCourseName() {
-		return courseName;
-	}
+	
+	
 }
---
+++ src/main/java/edu/handong/analysis/utils/Utils.java
@@ -0,0 +1,12 @@
+package edu.handong.analysis.utils;
+
+import java.util.ArrayList;
+
+public class Utils {
+
+	public static ArrayList<String> getLines(String dataPath, boolean b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
